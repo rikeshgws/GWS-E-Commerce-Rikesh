2437a643432d450be64399fd50f3b55ac65dbcab
RIKESHWARAN M <rikeshgws@gmail.com>
2026-06-28 18:17:00 +0530

    Update Vite configuration to use preview settings and adjust plugin initialization

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -2,9 +2,12 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 3000,
-    open: true
+  plugins: [react],
+  preview: {
+    host: '0.0.0.0',
+    port: Number(process.env.PORT) || 4173,
+    allowedHosts: [
+      'rikesh-get-me-gatway.up.railway.app'
+    ]
   }
 })
